8a5b913676191314595c19c34d2d68609dffca3e
sheena-dullete <sheenadullete1226@gmail.com>
2025-06-08 20:59:37 +0800

    Revert "added the option to re-enter name"
    This reverts commit 90d2f7d43c4f2f195c216d10eba69369e2f4f421.

--- Program.cs
+++ Program.cs
@@ -6,15 +6,6 @@ class Program
         Console.WriteLine("WELCOME!");
         string name;
         GetInput(out name);
-
-        Console.WriteLine("Enter name again? (y/n): ");
-        string choice = Console.ReadLine();
-
-        if (choice.ToLower() != "y")
-        {
-            return;
-        }
-        GetInput(out name);
     }
 
     static void GetInput(out string name)
