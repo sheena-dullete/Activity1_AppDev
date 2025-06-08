fd24d0c458589f972fba2adb1818a7c1f19164f3
sheena-dullete <sheenadullete1226@gmail.com>
2025-06-08 21:30:57 +0800

    added checkcourse and ask function

--- Program.cs
+++ Program.cs
@@ -4,15 +4,53 @@ class Program
     static void Main()
     {
         Console.WriteLine("Hello! Tell us about yourself.");
-        string name;
-        GetInput(out name);
+        string name, course;
+        GetInput(out name, out course);
+        checkCourse(course);
+        AskContinue();
     }
 
-    static void GetInput(out string name)
+    static void GetInput(out string name, out string course)
     {
         Console.WriteLine("\nEnter your name: ");
         name = Console.ReadLine() ?? "Anonymous";
 
+        Console.WriteLine("\nEnter your Course (BSCS, BSIT): ");
+        course = Console.ReadLine() ?? "Undeclared";
+
         Console.WriteLine("Hello, " + name + "!");
     }
+
+    static void checkCourse(string course)
+    {
+        if (course.ToLower() == "bscs")
+        {
+            Console.WriteLine("You're in Computer Science.");
+        }
+        else if (course.ToLower() == "bsit")
+        {
+            Console.WriteLine("You're in Information Technology.");
+        }
+        else
+        {
+            Console.WriteLine("Course Invalid");
+        }
+    }
+
+    static void AskContinue()
+    {
+        Console.WriteLine("\nWould you like to input again? (y/n): ");
+        string answer = (Console.ReadLine() ?? "").Trim().ToLower();
+
+        if (answer == "y")
+        {
+            Console.Clear();
+            Main();
+        }
+        else
+        {
+            Console.WriteLine("\nThank you! Program ended.");
+        }
+    }
+
 }
