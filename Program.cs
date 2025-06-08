f3a80706124536b62e82b1cbc450bdc243df08a0
sheena-dullete <sheenadullete1226@gmail.com>
2025-06-08 20:49:44 +0800

    added getInput Function

--- Program.cs
+++ Program.cs
@@ -4,5 +4,15 @@ class Program
     static void Main()
     {
         Console.WriteLine("WELCOME!");
+        string name;
+        GetInput(out name);
+    }
+
+    static void GetInput(out string name)
+    {
+        Console.WriteLine("Enter name:");
+        name = Console.ReadLine();
+
+        Console.WriteLine("Hello, " + name + "!");
     }
 }
