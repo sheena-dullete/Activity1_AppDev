47354b2890937c103809d9121ccd1698ade1809a
sheena-dullete <sheenadullete1226@gmail.com>
2025-06-08 19:46:04 +0800

    Initial Commit

--- Program.cs
+++ Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("WELCOME!");
+    }
+}
